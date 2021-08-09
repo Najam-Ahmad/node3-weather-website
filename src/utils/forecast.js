b70eecaa9f5440c9bc0a34f7534414957a714c1f
Ahmad <najam.ahmad@sap.com>
2021-08-09 13:49:08 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=c42b8564d4f1efc973faeecdd5a6d8c0&query=' + latitude + ',' + longitude + '&units=f'
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find the location.', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out." )
+    }
+})
+
+}
+
+module.exports = forecast
