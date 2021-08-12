478fcb33cb10cf1f5bd520829f3f52f4d3d33fad
Ahmad <najam.ahmad@sap.com>
2021-08-12 09:37:23 +0530

    Add humdity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find the location.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out." )
+            callback(
+                undefined,
+                body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + 
+                body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%."
+                 )
     }
 })
 
